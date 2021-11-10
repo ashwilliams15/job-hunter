d867cbe1b1549054ee7d2ff28401d329691b735a
Ashley Williams <ashleyswilliams01@gmail.com>
2021-11-10 13:59:00 -0700

    feat: adds window alert to add learned tech to resume

--- client/components/Technologies.js
+++ client/components/Technologies.js
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTechnologies, putTechnology } from '../store/technologies';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTechnologies, putTechnology } from "../store/technologies";
 
 const Technologies = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchTechnologies())
+    dispatch(fetchTechnologies());
   }, []);
 
-  const technologies = useSelector(state => state.technologies);
+  const technologies = useSelector((state) => state.technologies);
 
-  const unlearned = technologies.filter((technology) => technology.checked === false)
+  const unlearned = technologies.filter(
+    (technology) => technology.checked === false
+  );
 
-  //when checked, update the value in redux
+  //when checked, update the value in redux/backend
   const handleCheck = (technology) => {
-    dispatch(putTechnology(technology))
-  }
-  //@todo conditional render all checkboxes that are set to false
+    dispatch(putTechnology(technology));
+    window.alert("Don't forget to add to your resume!");
+  };
   return (
     <div>
       <div className="technologies">
@@ -25,18 +27,18 @@ const Technologies = () => {
         <div>
           {unlearned.map((technology) => (
             <div key={technology.id}>
-            <input
-              type="checkbox"
-              checked={false}
-              onChange={() => handleCheck(technology)}
-            />
-            <label htmlFor={technology.id}>{technology.name}</label>
+              <input
+                type="checkbox"
+                checked={false}
+                onChange={() => handleCheck(technology)}
+              />
+              <label htmlFor={technology.id}>{technology.name}</label>
             </div>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Technologies;
