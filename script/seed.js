1fd5294734f5df8620b9d67485f2b44c952a6454
Ashley Williams <ashleyswilliams01@gmail.com>
2021-11-24 07:50:54 -0700

    adds unit tests for contacts routes

--- script/seed.js
+++ script/seed.js
@@ -1,25 +1,44 @@
-'use strict'
+"use strict";
 
-const {db, models: { Technologies, Companies } } = require('../server/db')
+const {
+  db,
+  models: { Technologies, Companies, Contacts },
+} = require("../server/db");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   const technologies = await Promise.all([
-    Technologies.create({ name: 'Typescript', checked: false })
-  ])
+    Technologies.create({ name: "Typescript", checked: false }),
+  ]);
 
   const companies = await Promise.all([
-    Companies.create({ name: 'Asana', notes: 'team-based work management application' }),
-    Companies.create({ name: 'Slack', notes: 'platform for team management and communication '})
-  ])
+    Companies.create({
+      name: "Asana",
+      notes: "team-based work management application",
+    }),
+    Companies.create({
+      name: "Slack",
+      notes: "platform for team management and communication ",
+    }),
+  ]);
 
-  console.log(`seeded successfully`)
+  const contacts = await Promise.all([
+    Contacts.create({
+      firstName: "Cindy",
+      lastName: "Li",
+      company: "Shopify",
+      title: "Senior Software Developer",
+      linkedIn: "https://www.linkedin.com/in/ilydnic/",
+    }),
+  ]);
+
+  console.log(`seeded successfully`);
   // return {
   //   tasks: {
   //     firstTask: tasks[0]
@@ -33,16 +52,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -52,8 +71,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
---
+++ server/api/contacts.spec.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const request = require("supertest");
+const seed = require("../../script/seed");
+const app = require("../app");
+
+describe.only("Contacts Routes", () => {
+  beforeEach(async () => {
+    await seed();
+  });
+
+  describe("GET /contacts", () => {
+    it("returns all contacts from the database", async () => {
+      const res = await request(app).get("/api/contacts").expect(200);
+      expect(res.body.length).to.be.at.least(1);
+    });
+  });
+
+  describe("POST /contacts", () => {
+    it("creates a new technology in db", async () => {
+      const res = await request(app)
+        .post("/api/contacts")
+        .send({ firstName: "Test", lastName: "Contact" });
+      expect(res.body.firstName).to.equal("Test");
+    });
+  });
+});
